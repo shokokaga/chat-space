542454ce897fb28dbe35531177883a802926c46c
shokokaga <shoko.kaga@di-v.co.jp>
2020-05-30 13:16:31 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+>>>>>>> Stashed changes
 end

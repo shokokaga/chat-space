2f15329a2d6905ef16d7b58414f2f3e54f42f893
shokokaga <shoko.kaga@di-v.co.jp>
2020-05-30 19:16:24 +0900

    create

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   devise_for :users
-  root "messages#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
-
-
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index]
+  end
 end
